134bed8f165dab54b3d9245171b5ac3c0ddce1f9
Nguyen Khanh Toan <khanhtoan.forwork@gmail.com>
2026-01-25 10:28:32 +0700

    train fusion

--- check_tokens.py
+++ check_tokens.py
@@ -5,7 +5,7 @@ model_path = "/kaggle/input/best-modelll/lora_llm"
 try:
     tokenizer = AutoTokenizer.from_pretrained(model_path)
     
-    words = ["Unsure", "Neutral", "unsure", "true", "TRUE", "True", "false", "FALSE",  "false",  "False"]
+    words = ["Unsure", "Not", "unsure", "true", "TRUE", "True", "false", "FALSE",  "false",  "False"]
     for word in words:
         tokens = tokenizer(word, add_special_tokens=False)["input_ids"]
         print(f"Word: '{word}' -> Tokens: {tokens} (Count: {len(tokens)})")
